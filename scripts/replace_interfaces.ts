4331734e0902b53f3eb8b81a4f15d1f224d33cf0
Kuss <2360314753@qq.com>
2021-11-05 08:32:08 +0800

    :sparkles: feat: replace_interfaces -> commit changes

--- scripts/replace_interfaces.ts
+++ scripts/replace_interfaces.ts
@@ -1,11 +1,20 @@
 import path from 'path'
 import fs from 'fs'
+import { execSync } from 'child_process'
 
 const DIST_DIR = path.join(__dirname, '../dist')
 
 const README_PATH = path.join(__dirname, '../README.md')
 
 const T = '```'
+
+const gitCommit = () => {
+  const out = execSync('git status')
+  if (out.toString().includes('modified:   README.md')) {
+    execSync('git add README.md')
+    execSync('git commit -m "update: README.md#Interfaces"')
+  }
+}
 
 const main = () => {
   const indexDts = path.join(DIST_DIR, 'index.d.ts')
@@ -30,6 +39,8 @@ ${githubDtsContent}${T}
   fs.writeFileSync(README_PATH, replaced, { encoding: 'utf8' })
 
   console.log(`gdl: Replaced README.md#Interfaces`)
+
+  gitCommit()
 }
 
 main()
