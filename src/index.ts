279ff1e5f4642725b16b29f884a910d614a4f630
Kuss <2360314753@qq.com>
2021-11-05 08:24:21 +0800

    :bulb: update: index.ts -> typo

--- src/index.ts
+++ src/index.ts
@@ -20,14 +20,15 @@ export type Callback = (dir: string) => void | Promise<void>
 
 /**
  * The same as download above, but with two checking callbacks.
- * Will check targetDir is exists, 
+ * Will check if targetDir is exists, 
  * if exists and not empty, will try to call existsCallback, 
  *  defaults to remove targetDir and mkdir targetDir.
  * if not exists, will try to call notExistsCallback,
  *  defaults to mkdir targetDir.
  * @param url 
  * @param targetDir 
- * @param ifExistsCallback 
+ * @param ifExistsCallback
+ * @param ifNotExistsCallback
  */
 export async function downloadWithCheck(
   url: string,
