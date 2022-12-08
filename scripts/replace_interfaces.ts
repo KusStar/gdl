4473f1917adfa7f7ddbce2e2367e3253e325e2fb
Kuss <kussssss@outlook.com>
2022-12-08 18:03:07 +0800

    chore: update scripts/replace_interfaces

--- scripts/replace_interfaces.ts
+++ scripts/replace_interfaces.ts
@@ -1,6 +1,6 @@
-import path from 'path'
-import fs from 'fs'
 import { execSync } from 'child_process'
+import fs from 'fs'
+import path from 'path'
 
 const DIST_DIR = path.join(__dirname, '../dist')
 
@@ -18,9 +18,7 @@ const gitCommit = () => {
 
 const main = () => {
   const indexDts = path.join(DIST_DIR, 'index.d.ts')
-  const githubDts = path.join(DIST_DIR, 'github.d.ts')
   const indexDtsContent = fs.readFileSync(indexDts, 'utf8')
-  const githubDtsContent = fs.readFileSync(githubDts, 'utf8')
   const readmeContent = fs.readFileSync(README_PATH, 'utf8')
 
   const REGEX = /## Interfaces.*## Thanks/gms
@@ -30,15 +28,11 @@ ${T}ts
 // index.d.ts
 ${indexDtsContent}${T}
 
-${T}ts
-// github.d.ts
-${githubDtsContent}${T}
-
 ## Thanks`)
 
   fs.writeFileSync(README_PATH, replaced, { encoding: 'utf8' })
 
-  console.log(`gdl: Replaced README.md#Interfaces`)
+  console.log('gdl: Replaced README.md#Interfaces')
 
   gitCommit()
 }
